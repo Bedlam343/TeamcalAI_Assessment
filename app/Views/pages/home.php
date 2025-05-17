524f0327bf345dfd45ae8b8c601823585cd8164c
bedlam343 <jagik22@gmail.com>
2025-05-16 21:43:54 -0700

    loading data spinner

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -164,16 +164,28 @@
     </script>
     
     <script type="text/babel">
-      function Form() {
+      function Spinner() {
+        return (
+          <div role="status">
+            <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        );
+      }
+
+      function Form({ onSubmit }) {
         return (
           <div>
             <p 
-              className="text-4xl text-center underline decoration-2 underline-offset-4 decoration-blue-600 decoration-wavy"
+              className="text-4xl text-center underline decoration-2 underline-offset-4 decoration-blue-400 decoration-wavy"
             >
               Calendly Schedule Extractor
             </p>
 
-            <form action="/" method="post" className="mt-8">
+            <form onSubmit={onSubmit} action="/" method="post" className="mt-8">
               <div className="flex flex-col items-center">
                 <div>
                   <label htmlFor="calendly_url" className="block font-bold text-stone-700">Calendly Link</label>
@@ -198,14 +210,24 @@
         )
       }
 
-      function Home() {            
-          return (
-            <div className="my-8">
-              <Form />
-              <div className="h-[40px]" />
-              {data_arrived && <Availability />}
-            </div>
-          );
+      function Home() {
+        const [fetchingData, setFetchingData] = React.useState(false);
+
+        function onFormSubmit() {
+          setFetchingData(true);
+        }
+
+        return (
+          <div className="my-8">
+            <Form onSubmit={onFormSubmit} />
+            <div className="h-[40px]" />
+            {fetchingData && <div className="flex flex-col items-center gap-3">
+              <Spinner />
+              <p className="text-stone-800">Fetching availability data...</p>
+            </div>}
+            {data_arrived && <Availability />}
+          </div>
+        );
       }
 
       ReactDOM.createRoot(document.getElementById('react-root')).render(<Home />);
